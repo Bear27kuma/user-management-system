82f0d7f61d7e67c9cc0bf802ffccc29c84693bd4
Yuta Okuma <y.dream.nest@icloud.com>
2022-02-01 13:05:34 +0900

    mysqlとのコネクション設定を追加

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true
 });
 
 pool.getConnection((err, connection) => {
